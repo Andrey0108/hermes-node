76cfc74cd91aac07f8a2e1924e9efe0c6b7a4c84
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-09-03 11:41:23 -0500

    review: delete auth

--- src/app.js
+++ src/app.js
@@ -4,17 +4,17 @@ import { createAuthRouter } from './routes/auth.routes.js'
 import { createCustomersRouter } from './routes/reservations/customers.routes.js'
 import { createPaysRouter } from './routes/pays/pays.routes.js'
 // models
-import { AuthModel } from './models/auth.model.js'
+// import { AuthModel } from './models/auth.model.js'
 import { CustomersModel } from './models/reservations/customers.model.js'
 import { PaysModel } from './models/pays/pays.model.js'
 import './config/db.mongo.js'
 
 const server = new Server()
-const authModel = new AuthModel()
+// const authModel = new AuthModel()
 const customersModel = new CustomersModel()
 const paysModel = new PaysModel()
 
-server.app.use('/auth', createAuthRouter({ authModel }))
+// server.app.use('/auth', createAuthRouter({ authModel }))
 server.app.use('/customers', createCustomersRouter({ customersModel }))
 server.app.use('/pays', createPaysRouter({ paysModel }))
 
