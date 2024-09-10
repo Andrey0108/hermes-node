bc6e2a6061f1e8b822706cd4d061552846cf6b99
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-09-09 20:35:15 -0500

    modify customersSchema

--- src/schemas/mongodb/reservations/customers.schema.js
+++ src/schemas/mongodb/reservations/customers.schema.js
@@ -1,18 +1,18 @@
 import { Schema, model } from 'mongoose'
 const customersSchema = new Schema({
-  id_customer: Number,
-  id_user: Number,
-  name: String,
-  lastName: String,
-  phone: String,
-  address: String,
-  country: Number,
-  departament: Number,
-  municipality: Number,
-  sex: String,
-  bloodType: String,
-  eps: String,
-  state: Boolean
+  id_customer: { Number, required: true },
+  id_user: { Number, required: true },
+  name: { String, required: true },
+  lastName: { String, required: true },
+  phone: { String, required: true },
+  address: { String, required: true },
+  country: { Number, required: true },
+  departament: { Number, required: true },
+  municipality: { Number, required: true },
+  sex: { String, required: true, enum: ['h', 'm'] },
+  bloodType: { String, required: true, enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'] },
+  eps: { String, required: true },
+  state: { Boolean, default: true }
 })
 
 export const CustomersSchema = model('Customers', customersSchema)
