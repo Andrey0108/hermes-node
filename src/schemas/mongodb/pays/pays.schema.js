dafeede9988ba88edcdc236162a31f08652a56d7
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-09-09 20:44:05 -0500

    modify paysSchema

--- src/schemas/mongodb/pays/pays.schema.js
+++ src/schemas/mongodb/pays/pays.schema.js
@@ -1,12 +1,12 @@
 import { Schema, model } from 'mongoose'
 
 const paysSchema = new Schema({
-  id_pay: Number,
-  id_reservation: Number,
-  date_pay: Date,
-  price: Number,
-  voucher: String,
-  status: String
+  id_pay: { Number, required: true },
+  id_reservation: { Number, required: true },
+  date_pay: { Date, required: true },
+  price: { Number, required: true },
+  voucher: { String, required: true },
+  status: { String, required: true, enum: ['R', 'P', 'N', 'A'] }
 })
 
 export const PaysSchema = model('Pays', paysSchema)
