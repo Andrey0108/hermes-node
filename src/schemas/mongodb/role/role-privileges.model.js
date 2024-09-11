edca07a2805567bfaaf30fa58c2b77c51af2dae7
Juandrk07 <juan_ptorol@soy.sena.edu.co>
2024-09-11 06:56:20 -0500

    Resolved conflicts

--- src/schemas/mongodb/role/role-privileges.model.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose'
-
-const rolePrivilegesSchema = new Schema({
-  id_role_privilege: { type: Number, required: true },
-  id_role: { type: Number, required: true },
-  id_privilege: { type: Number, required: true }
-})
-
-export const RolePrivilegesSchema = model('RolePrivileges', rolePrivilegesSchema)
--- src/schemas/mongodb/service/services.js
+++
@@ -1,9 +0,0 @@
-import { Schema } from "mongoose";
-
-export const permissionsSchema = Schema({
-  id_service: String,
-  id_categoryService: String,
-  name: String,
-  price: String,
-  status: String,
-});
