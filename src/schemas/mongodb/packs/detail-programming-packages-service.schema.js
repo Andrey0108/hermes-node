525854be90248c9b65dcb7e388d2be3d74b6833e
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2024-09-19 14:55:03 -0500

    rename on reservations

--- src/schemas/mongodb/packs/detail-programming-packages-service.schema.js
+++ src/schemas/mongodb/packs/detail-programming-packages-service.schema.js
@@ -1,8 +1,8 @@
 import { Schema, model } from 'mongoose'
 
 const detailProgrammingPackagesServiceSchema = new Schema({
-  id_detail_programming_package_service: { type: Number, required: true },
-  id_detail_programming_package: { type: Number, required: true },
+  idDetailProgrammingPackage_service: { type: Number, required: true },
+  idDetailProgrammingPackage: { type: Number, required: true },
   id_detail_package_service: { type: Number, required: true },
   quantity: { type: Number, required: true },
   price_service: { type: Number, required: true }
